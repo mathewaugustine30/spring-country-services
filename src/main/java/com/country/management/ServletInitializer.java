25e6be677a66ddcdc5d1a9cdef32167d116901d8
mathewaugustine30 <mathewaugustine30@gmail.com>
2022-03-08 13:18:39 +0530

    Added Servlet Initializer class for Jenkins Excluded ServletInitialize.java from jococo reports

---
+++ src/main/java/com/country/management/ServletInitializer.java
@@ -0,0 +1,12 @@
+package com.country.management;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CountryServicesApplication.class);
+    }
+}
